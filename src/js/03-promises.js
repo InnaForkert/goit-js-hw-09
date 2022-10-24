6d1554c92cde6c7e5f0e577a46498a8e8c8e24d6
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-25 01:04:31 +0300

    what is going on

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+const form = document.querySelector('.form');
+let position = 1;
+let promiseInterval;
+let whaat;
+// console.log(delay.value);
+
+form.addEventListener('submit', handleSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  console.log(position);
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay.value}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay.value}ms`);
   }
+  if (position === amount.value) {
+    clearInterval(promiseInterval);
+    position = 1;
+  }
+  increasePosition();
+}
+
+function increasePosition() {
+  whaat = setInterval(() => {position += 1}, 0);
+}
+ 
+function handleSubmit(event) {
+  event.preventDefault();
+  setTimeout(() => {
+    promiseInterval = setInterval(createPromise, 1200, position, delay);
+  }, delay.value)
 }
