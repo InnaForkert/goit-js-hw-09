8a025a1e2a37efe411b7981de49c1cec158eab01
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-24 21:19:55 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timer;
+const body = document.querySelector('body');
 
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', colorStop);
+stopButton.setAttribute('disabled', '');
+
+function changeColor() {
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.setAttribute('disabled', '');
+    stopButton.removeAttribute('disabled');
+}
+
+function colorStop() {
+    clearInterval(timer);
+    stopButton.setAttribute('disabled', '');
+    startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
